dc7c87979db8e2d9a79eb3b81d0e1d420d82132a
arnau <arnau.vidal@gigaset.com>
2021-11-28 10:51:59 +0100

    Prettify with Bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -11,6 +12,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
