f2e2c1cf243e5f04c702eed0be80ae88a98e8f10
arnau <arnau.vidal@gigaset.com>
2021-12-20 06:23:48 +0100

    Display user info popup when hovering over user link

--- app/main/routes.py
+++ app/main/routes.py
@@ -139,3 +139,11 @@ def user(username: str):
     prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url, form=form)
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
